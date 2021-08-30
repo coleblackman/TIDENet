56068cd1719242cea0b31acb1bce1f53913dca5c
Cole Blackman <csb3ws@virginia.edu>
2021-08-30 15:38:59 -0400

    Update TIDENetBash.sh

--- TIDENetBash.sh
+++ TIDENetBash.sh
@@ -3,5 +3,6 @@ git clone https://github.com/hsharma35/dnnweaver2.git
 mv dnnweaver2/dnnweaver2 dnnweaver
 rm -rf dnnweaver2
 mv dnnweaver dnnweaver2
+pip install graphviz pandas
 
 python TIDENetHelloWorld.py
