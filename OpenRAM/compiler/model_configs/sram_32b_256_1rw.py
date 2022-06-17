cec75902f5cfd859fa1fe81db9b9a0b2cd3ec53e
Cole Blackman <coleblax@gmail.com>
2022-06-17 08:22:19 -0700

    removed old OpenRAM module

--- OpenRAM/compiler/model_configs/sram_32b_256_1rw.py
+++
@@ -1,8 +0,0 @@
-from shared_config import *
-word_size = 32
-num_words = 256
-
-output_extended_config = True
-output_datasheet_info = True
-netlist_only = True
-nominal_corner_only = True
--- OpenRAM/compiler/model_configs/sram_64b_512_1rw.py
+++
@@ -1,8 +0,0 @@
-from shared_config import *
-word_size = 64
-num_words = 512
-
-output_extended_config = True
-output_datasheet_info = True
-netlist_only = True
-nominal_corner_only = True
